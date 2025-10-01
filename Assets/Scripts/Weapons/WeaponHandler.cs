3d791e1fa196c0347187dadb83fb627e4304db8f
daniel-davenport <danieladavenport2020@gmail.com>
2025-10-01 15:45:57 -0400

    Lock on indicator lifetime reduction

--- Assets/Scripts/Weapons/WeaponHandler.cs
+++ Assets/Scripts/Weapons/WeaponHandler.cs
@@ -150,7 +150,7 @@ public class WeaponHandler : MonoBehaviour
         yield return new WaitUntil(() => lockedOnEnemies[slot] == null);
 
         // waits for a bit for the projectile to travel
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.25f);
 
         // destroy the indicator
         Destroy(indicators[slot]);
