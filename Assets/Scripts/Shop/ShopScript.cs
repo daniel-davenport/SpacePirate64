4b04a356fe4e42b21bd0492d070dd49e5f27b4d4
daniel-davenport <danieladavenport2020@gmail.com>
2025-10-27 17:07:45 -0400

    Shop JSON setup & repair purchasing

--- Assets/Scripts/Shop/ShopScript.cs
+++ Assets/Scripts/Shop/ShopScript.cs
@@ -1,16 +1,47 @@
+using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using System.IO;
 
 public class ShopScript : MonoBehaviour
 {
     [Header("References")]
     public GameObject shopUI;
+    public PlayerController playerController;
     public LevelDirector levelDirector;
     public ShopUIEvents shopUIEvents;
+
+    [Header("Stats")]
+    public int repairCost;
+    private TextAsset itemListJson;
+
+    // setting up loot table .json file
+    [System.Serializable]
+    public class Item
+    {
+        public string name;
+        public string displayName;
+        public int rarity; // lower number = less rare
+        public int cost;
+    }
+
+    [System.Serializable]
+    public class ItemList
+    {
+        public Item[] items;
+    }
+
+    [SerializeField]
+    public ItemList allItemsList = new ItemList();
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         shopUIEvents = shopUI.GetComponent<ShopUIEvents>();
+        shopUIEvents.playerController = playerController;
+
+        LoadWeaponJson();
     }
 
     // Update is called once per frame
@@ -19,10 +50,39 @@ public class ShopScript : MonoBehaviour
         
     }
 
+
+    private void LoadWeaponJson()
+    {
+        // reading enemy info from the physical json instead of a compiled one
+        string itemFilePath = Path.Combine(Application.streamingAssetsPath, "ItemData.json");
+        if (File.Exists(itemFilePath))
+        {
+            string fileContent = File.ReadAllText(itemFilePath);
+            itemListJson = new TextAsset(fileContent);
+
+            allItemsList = JsonUtility.FromJson<ItemList>(itemListJson.text);
+        }
+        else
+        {
+            print("ERROR: ITEM DATA FILE NOT FOUND.");
+        }
+
+
+        // manually printing every item at the end
+        /*
+        for (int i = 0; i < allItemsList.items.Length; i++) { 
+            print(allItemsList.items[i].name);
+        }
+        */
+
+    }
+
     // called at a level end, creates the shop's stock and shows the shop hud.
     public void RefreshShop()
     {
+        // generate the shop's stock using RNG to determine the rarity of each item
 
+        // fire it to the shopui so it can display the text for every slot
 
 
         // at the end, show the shop ui
@@ -44,6 +104,35 @@ public class ShopScript : MonoBehaviour
     public void RepairShip()
     {
         print("clicked repair");
+        int playerHP = playerController.playerHealth;
+        int playerMaxHP = playerController.maxHealth;
+        int playerHeldScrap = playerController.heldScrap;
+
+        // they're allowed to heal
+        if (playerHP < playerMaxHP)
+        {
+            // they have enough to buy a repair
+            if (playerHeldScrap >= repairCost)
+            {
+                print("can repair");
+                // increase their HP and reduce their scrap by the repair cost
+                playerController.playerHealth += 1;
+                playerController.heldScrap -= repairCost;
+
+            } 
+            else
+            {
+                // too poor
+                print("poor");
+            }
+
+
+        }
+        else
+        {
+            print("health already max");
+        }
+
     }
 
     // compares the current held scrap with the cost of the item
--- Assets/Scripts/Shop/ShopUIEvents.cs
+++ Assets/Scripts/Shop/ShopUIEvents.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
 
 public class ShopUIEvents : MonoBehaviour
 {
     [Header("References")]
     public ShopScript shopScript;
+    public PlayerController playerController;
     public List<Button> shopButtons = new List<Button>();
-    
+    public List<Label> itemTexts = new List<Label>();
 
     private UIDocument document;
     private Button repairButton;
     private Button closeButton;
+
+    private Label repairCost;
+    private Label hullHealthLabel;
+    private Label scrapAmount;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,11 +28,20 @@ public class ShopUIEvents : MonoBehaviour
         repairButton = document.rootVisualElement.Q("RepairButton") as Button;
         closeButton = document.rootVisualElement.Q("CloseButton") as Button;
 
+        // getting labels
+        hullHealthLabel = document.rootVisualElement.Q("HullHealth") as Label;
+        scrapAmount = document.rootVisualElement.Q("ScrapAmount") as Label;
+        repairCost = document.rootVisualElement.Q("RepairCost") as Label;
+
         repairButton.RegisterCallback<ClickEvent>(OnRepairClick);
         closeButton.RegisterCallback<ClickEvent>(OnCloseClick);
 
         // list of all buttons
         shopButtons = document.rootVisualElement.Query<Button>().ToList();
+
+        // list of all item texts
+        itemTexts = document.rootVisualElement.Query<Label>().ToList();
+
 
         int slot = 1;
         foreach (Button button in shopButtons)
@@ -40,6 +56,21 @@ public class ShopUIEvents : MonoBehaviour
             }
 
         }
+
+        // iterating through itemtexts backwards to remove any non-itemtext labels
+        for (int i = itemTexts.Count - 1; i >= 0; i--)
+        {
+            if (itemTexts[i].name != "ItemText")
+            {
+                itemTexts.RemoveAt(i);
+            } else
+            {
+                // test case to show it really exists (remember that the order in the UI Builder matters)
+                itemTexts[i].text = (i).ToString();
+            }
+        }
+
+
 
         // disabling the shop so it doesnt appear
         HideDocument();
@@ -103,6 +134,14 @@ public class ShopUIEvents : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerController != null)
+        {
+            // shop stats
+            repairCost.text = "[" + shopScript.repairCost + "] SCRAP";
+
+            // player stats
+            hullHealthLabel.text = "HULL: " + playerController.playerHealth + "/" + playerController.maxHealth;
+            scrapAmount.text = playerController.heldScrap.ToString();
+        }
     }
 }
